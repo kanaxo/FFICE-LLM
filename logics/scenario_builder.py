e3a4c68dba0c463993805613c38fd58044b741c0
kanaxo <60850434+kanaxo@users.noreply.github.com>
2024-10-28 00:36:13 +0800

    Revert "changed requirements.txt and add disclaimer"
    This reverts commit eab8e6c2eaf3d44aab77f2866e0b0f9dbdd244e4.

--- logics/scenario_builder.py
+++ logics/scenario_builder.py
@@ -1,8 +1,5 @@
 import os
 # from dotenv import load_dotenv
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 from crewai import Agent, Task, Crew
 from crewai_tools import BaseTool
